df1f6f231e2260561afb8d0e5995b82d39905a91
teboho <tebohomakhalemele@outlook.com>
2024-03-08 16:13:48 +0200

    Added a unit of work to handle the daving of changes

---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Repository object
+        /// </summary>
+        IDeveloperRepository Developers { get; }
+
+        /// <summary>
+        /// Repository object
+        /// </summary>
+        IProjectRepository Projects { get; }
+
+        /// <summary>
+        /// Save changes to the database
+        /// </summary>
+        /// <returns>save code</returns>
+        int Complete();
+    }
+}
