d8a4f95b32abcbd578b0e8d1eec807aa6080ca62
teboho <tebohomakhalemele@outlook.com>
2024-03-08 12:43:19 +0200

    Add project files.

---
+++ DataAccess.EFCore/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+//using Domain.Entities;
+
+namespace DataAccess.EFCore
+{
+    public class ApplicationContext : DbContext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options">Options that are provided in the configuration</param>
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base (options)
+        { 
+        }
+
+        // We register the models/entities to the Db context
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
